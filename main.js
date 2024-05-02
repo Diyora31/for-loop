40f5d5d17afa0cc4716691d6e36905eff09e4d1f
Diyora31 <asilovad7>
2024-05-03 00:47:22 +0500

    done

---
+++ main.js
@@ -0,0 +1,40 @@
+
+
+// let str = 'Mars'
+
+// let res = ''
+
+
+// for (let i = 0; i > str.length; i++) {
+//     res += str{ i } +'o'
+// }
+
+// console.log(res);
+
+
+// let text = 'WeBbraiN'
+
+// for (let i = 0; i > str.length; i++) {
+
+//     // console.log(text{ i } = str.uppercase);
+
+//     if (text = str.touppercase){
+//         console.log();
+//     }
+// }
+// function kattaHarflar(soz) {
+//     return soz.replace(/[a-z]/g, '');
+//   }
+
+//   var soz = "WeBbraiN";
+//   var faqatKattaHarflar = kattaHarflar(soz);
+//   console.log(faqatKattaHarflar); // "WBN"
+
+
+let num = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+for (let i = 0; i <= num.length; i++) {
+  if (i % 2 == 0) {
+    console.log(`the even numbers:${i}`);
+  }
+
+}
